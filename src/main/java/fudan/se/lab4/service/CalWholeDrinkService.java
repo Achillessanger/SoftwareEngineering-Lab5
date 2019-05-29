d53aa6b728a2b79ad69a074514ba90fb63c4e958
achillessanger <1359848409@qq.com>
2019-05-29 13:32:32 +0800

    给calWholeDrinkService接口加了描述

--- src/main/java/fudan/se/lab4/service/CalWholeDrinkService.java
+++ src/main/java/fudan/se/lab4/service/CalWholeDrinkService.java
@@ -2,6 +2,12 @@ package fudan.se.lab4.service;
 
 import fudan.se.lab4.dto.OrderItem;
 
+
 public interface CalWholeDrinkService {
+    /**
+     *
+     * @param orderItem 订单内单个商品
+     * @return 换算后按当前货币种类计算好的价格
+     */
     Double wholePrice(OrderItem orderItem);
 }
